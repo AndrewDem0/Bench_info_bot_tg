4aabce230cb5f00e619fd1c58eaa43ccb003b879
AndrewDem0 <144722259+AndrewDem0@users.noreply.github.com>
2025-08-29 17:01:02 +0300

    done admin "Input received"

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 PDF_PATH = "models/history.pdf"
 YOUTUBE_LINK = "https://youtu.be/YU22jN1OoZ4?si=j3GNXqeIl_Pye28K"
+ADMIN_PASSWORD_HASH = "fea12dcd51cf6778cbf2c5f0fad3f26300c07aab9fc7277d734c6c9e8d5c9aa8"
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import os
 import config
+import hashlib
+import json
 from dotenv import load_dotenv
 from typing import Final
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes, CallbackQueryHandler
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, KeyboardButton
+
+admin_waiting_users : set = set()
 
 load_dotenv()
 Token: Final = os.getenv('TOKEN')
@@ -25,10 +29,6 @@ async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
         "Привіт! 👋 Натисни кнопку нижче, щоб почати:",
         reply_markup = reply_markup
     )
-#
-async def custom_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('Кастомна команда')
-#
 async def history_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [[
         InlineKeyboardButton("📄 PDF", callback_data="show_pdf"),
@@ -40,11 +40,24 @@ async def history_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
         'Оберіть формат історії:',
         reply_markup = reply_markup
     )
+#
+async def admin_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.message.from_user
+    user_id =update.message.from_user.id
+    admin_waiting_users.add(user_id)
+    
+    print(f'User: ({user.id}, @{user.username}) tries to access admin panel')
+    
+    await update.message.reply_text('Введіть пароль для доступу до адмін-панелі:')
+#
+async def custom_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Кастомна команда')
+#
 
 # Responses
 def handle_response(text: str) -> str:
     processed: str = text.lower()
-
+    
     if "💬 зв'язатися з розробником" in processed:
         return "Шукайте за тегом👇\n" + "@F4076 "
     elif '🌐 громадська активність' in processed:
@@ -55,9 +68,13 @@ def handle_response(text: str) -> str:
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user = update.message.from_user
+    user_id: int = update.message.from_user.id
     message_type: str = update.message.chat.type
     text: str = update.message.text
-
+    
+    if user_id in admin_waiting_users:
+        return
+    
     print(f'User: ({user.id}, @{user.username}) in {message_type}: "{text}"')
 
     response = None  # <-- важливо!
@@ -76,7 +93,22 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if response:
         print('Bot: ', response)
         await update.message.reply_text(response)
-
+#
+async def handle_admin_password(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = update.message.from_user.id
+    text : str = update.message.text.strip()
+    
+    if user_id not in admin_waiting_users:
+        return
+    
+    hashed_input = hashlib.sha256(text.encode()).hexdigest()
+    if hashed_input == config.ADMIN_PASSWORD_HASH:
+        await update.message.reply_text("Пароль вірний ✅ Ви увійшли в адмін-панель")
+    else:
+        await update.message.reply_text("Невірний пароль ❌")
+    
+    admin_waiting_users.remove(user_id)
+    
 #
 async def button_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
@@ -111,10 +143,13 @@ if __name__ == '__main__':
     app.add_handler(CommandHandler('start', start_command))
     app.add_handler(CommandHandler('custom', custom_command))
     app.add_handler(CommandHandler('history', history_command))
+    app.add_handler(CommandHandler('admin', admin_command))
 
     # Messages
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_admin_password))
     app.add_handler(MessageHandler(filters.TEXT, handle_message))
     app.add_handler(CallbackQueryHandler(button_callback))
+
     # Errors
     app.add_error_handler(error)
 
